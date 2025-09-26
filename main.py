04ffcc90a66de6d57f2c823a7497785977c47e83
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-09-26 12:07:11 -0700

    S39 App7 152 Getting All the news of Today

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+## Newsapi.org API key 3586a06025c9449fbd48d8c78c2a9dad
+import requests
+from pprint import pprint
+
+
 class NewsFeed:
 
     def __init__(self, data):
@@ -5,3 +10,8 @@ class NewsFeed:
 
     def get(self):
         pass
+
+
+response = requests.get("https://newsapi.org/v2/everything?q=tesla&from=2025-08-26&sortBy=publishedAt&apiKey=3586a06025c9449fbd48d8c78c2a9dad")
+content = response.text
+pprint(content)
