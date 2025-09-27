bed48caac24fc1ad5170a3e2cc1e969b655d75e8
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-09-27 10:00:46 -0700

    S40 App7 156 implementing the News Feed Class

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ from pprint import pprint
 
 
 class NewsFeed:
+    """Representing multiple news titles and links as a single string
+    """
 
-    def __init__(self, data):
-        self.data = data
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "3586a06025c9449fbd48d8c78c2a9dad"
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
 
     def get(self):
-        pass
+        url = (f"{self.base_url}"
+               f"qInTitle={self.interest}&"
+               f"from={self.from_date}&"
+               f"to={self.to_date}&"
+               f"language={self.language}&"
+               f"apiKey={self.api_key}")
 
-url = ("https://newsapi.org/v2/everything?"
-       "qInTitle=meditation&"
-       "from=2025-09-25&"
-       "to=2025-09-27&"
-       "language=en&"
-       "apiKey=3586a06025c9449fbd48d8c78c2a9dad")
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
 
-response = requests.get(url)
-content = response.json()
-articles = content['articles']
+        email_body = ''
+        for article in articles:
+            email_body = email_body + article['title'] + '\n' + article['url'] + '\n\n'
 
-email_body = ''
-for article in articles:
-    email_body = email_body + article['title'] + '\n' + article['url'] + '\n\n'
+        return email_body
 
-pprint(email_body)
+news_feed = NewsFeed(interest='nasa', from_date='2025-09-25', to_date='2025-09-27', language='en')
+pprint(news_feed.get())
+
+
