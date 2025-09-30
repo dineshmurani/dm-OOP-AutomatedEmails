eb8b1992f71a57f79567ae813c3afb7d6d8fd8fb
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-09-30 11:17:35 -0700

    S42 163 Refactoring our Code

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ from news import NewsFeed
 import datetime
 import time
 
+
+def send_email():
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    news_feed = NewsFeed(interest=row["interest"],
+                         from_date=yesterday,
+                         to_date=today)
+    email = yagmail.SMTP(user='pythoncourse1@gmail.com', password="python_pro_course_1")
+    email.send(to=row['email'],
+               subject=f'Your {row['interest']}  news for today!',
+               contents=f"Hi {row["name"]}\n See what's on about {row["interest"]} Today. \n{news_feed.get()}\nDinesh", )
+
+
 while True:
     if datetime.datetime.now().hour == 17 and datetime.datetime.now().minute == 43:
         df = pandas.read_excel('people.xlsx')
 
         for index, row in df.iterrows():
-            today = datetime.datetime.now().strftime('%Y-%m-%d')
-            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-            news_feed = NewsFeed(interest=row["interest"],
-                                 from_date=yesterday,
-                                 to_date=today)
-            email = yagmail.SMTP(user='pythoncourse1@gmail.com', password="python_pro_course_1")
-            email.send(to=row['email'],
-                       subject=f'Your {row['interest']}  news for today!',
-                       contents=f"Hi {row["name"]}\n See what's on about {row["interest"]} Today. \n{news_feed.get()}\nDinesh",)
+            send_email()
 
     time.sleep(60)
